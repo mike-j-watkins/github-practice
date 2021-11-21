d2ddb58675c779544cb5350ad913ace6338163e5
Michael Watkins <watkinsmichaeljames@gmail.com>
2021-11-21 13:41:28 -0500

    fleshed out logic

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 "use strict";
+
+const btnContainer = document.querySelector(".btn-container");
+const result = document.querySelector(".result");
+const playerWins = document.querySelector(".player-wins-num");
+const computerWins = document.querySelector(".computer-wins-num");
+const winner = document.querySelector(".winner");
+let playing = 1;
+
 function computerTurn(min, max) {
   let turn = Math.floor(Math.random() * (max - min)) + min;
   if (turn == 1) return "rock";
@@ -6,30 +14,46 @@ function computerTurn(min, max) {
   if (turn == 3) return "scissors";
 }
 
-let comp = computerTurn(1, 4);
-let playerTurn = prompt("insert rock paper or scissors");
-if (comp == playerTurn) console.log("draw");
-if (playerTurn == "rock") {
-  if (comp == "paper") {
-    console.log("You lose");
-  } else {
-    console.log("You win!");
+const calculateWinner = function (comp, playerTurn) {
+  if (comp == playerTurn) return { message: "draw" };
+  if (playerTurn == "rock") {
+    return comp == "scissors"
+      ? { winner: "player", message: "You win!" }
+      : { winner: "computer", message: "You lose!" };
+  }
+  if (playerTurn == "paper") {
+    return comp == "rock"
+      ? { winner: "player", message: "You win!" }
+      : { winner: "computer", message: "You lose!" };
+  }
+
+  if (playerTurn == "scissors") {
+    return comp == "paper"
+      ? { winner: "player", message: "You win!" }
+      : { winner: "computer", message: "You lose!" };
+  }
+};
+
+function playRound(e) {
+  if (playing) {
+    if (!e.target.classList.contains("btn")) return;
+    let playerTurn = e.target.dataset.choice;
+    let comp = computerTurn(1, 4);
+    let ans = calculateWinner(comp, playerTurn);
+    result.textContent = ans.message;
+
+    if (!ans.winner) return;
+    ans.winner == "player"
+      ? (playerWins.textContent = +playerWins.textContent + 1)
+      : (computerWins.textContent = +computerWins.textContent + 1);
+    if (+playerWins.textContent == 5) {
+      winner.textContent = "Player has won the game!";
+      playing--;
+    }
+    if (+computerWins.textContent == 5) {
+      winner.textContent = "Computer has won the game!";
+      playing--;
+    }
   }
 }
-if (playerTurn == "paper") {
-  if (comp == "rock") {
-    console.log("You win!");
-  } else {
-    console.log("You lose");
-  }
-}
-if (playerTurn == "scissors") {
-  switch (comp) {
-    case "rock":
-      console.log("You lose");
-      break;
-    case "paper":
-      console.log("You win");
-      break;
-  }
-}
+btnContainer.addEventListener("click", playRound);
